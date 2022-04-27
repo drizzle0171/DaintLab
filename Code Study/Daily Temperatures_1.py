0d8448affa3ee085f3d93dbd392c4189624a26e3
drizzle0171 <yongsandew@gmail.com>
2022-04-27 10:32:57 +0900

    ReportResult

--- Code Study/Daily Temperatures_1.py
+++ Code Study/Daily Temperatures_1.py
@@ -1,10 +1,16 @@
-class Solution:
-    def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
+from sqlalchemy import false, true
+
+
+temperatures = [73, 74, 75, 71, 69, 72, 76, 73]
+def dailyTemperatures(temperatures):
         stack = []
         result = [0]*len(temperatures)
         for i, current in enumerate(temperatures):
             while stack and current > temperatures[stack[-1]]:
+                print(stack, stack[-1], temperatures[stack[-1]])
                 index = stack.pop()
                 result[index] = i-index
             stack.append(i)            
         return result
+print(dailyTemperatures(temperatures))
+print([]==False)
---
+++ Code Study/Report result.py
@@ -0,0 +1,25 @@
+from collections import defaultdict
+id = ["muzi", "frodo", "apeach", "neo"]
+report = ["muzi frodo","apeach frodo","frodo neo","muzi neo","apeach muzi"]
+k = 3
+
+def solution(id_list, report, k):
+    count_d = defaultdict(int)
+    report_d = defaultdict(set)
+    answer = [0 for _ in range(len(id_list))]
+
+    for x in report:
+        person, reported = x.split()
+        report_d[person].add(reported)
+
+    for x in report_d.values():
+        for y in x:
+            count_d[y] += 1
+
+    for idx, x in enumerate(id_list):
+        for y in report_d[x]:
+            if count_d[y] >= k:
+                answer[idx] += 1
+
+    return answer
+print(solution(id, report, k))
