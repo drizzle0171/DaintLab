79e3fa372cb2811dc9b0e9e05c5f134ff06cd88a
drizzle0171 <yongsandew@gmail.com>
2022-11-25 22:43:24 +0900

    update

--- mirae.sh
+++ mirae.sh
@@ -22,24 +22,24 @@
 #   --hidden_size 128 \
 #   --pred_len 12 \
 #   --train_epochs 10 \
-#   --itr 1 --batch_size 128 --learning_rate 0.001 >logs/mirae/$model_name'_'mirae'_'final.log
+#   --itr 1 --batch_size 128 --learning_rate 0.0001 >logs/mirae/$model_name'_'mirae'_'final.log
 
-# # Linear
-# model_name=Linear
-# CUDA_VISIBLE_DEVICES=1 python -u run_longExp.py \
-#   --is_training 1 \
-#   --x_data_path ../Data/x_H.npy \
-#   --y_data_path ../Data/y_H.npy \
-#   --model_id mirae_Linear \
-#   --model $model_name \
-#   --data mirae \
-#   --des 'Exp' \
-#   --loss mse \
-#   --feature 'MS' \
-#   --seq_len 72 \
-#   --pred_len 12 \
-#   --train_epochs 10 \
-#   --itr 1 --batch_size 32 --learning_rate 0.005 >logs/mirae/$model_name'_'mirae'_'final.log
+# Linear
+model_name=Linear
+CUDA_VISIBLE_DEVICES=1 python -u run_longExp.py \
+  --is_training 1 \
+  --x_data_path ../Data/x_H.npy \
+  --y_data_path ../Data/y_H.npy \
+  --model_id mirae_Linear \
+  --model $model_name \
+  --data mirae \
+  --des 'Exp' \
+  --loss mse \
+  --feature 'MS' \
+  --seq_len 72 \
+  --pred_len 12 \
+  --train_epochs 10 \
+  --itr 1 --batch_size 32 --learning_rate 0.005 >logs/mirae/mirae_MS.log
 
 # # NLinear
 # model_name=NLinear
@@ -56,7 +56,7 @@
 #   --seq_len 72 \
 #   --pred_len 12 \
 #   --train_epochs 10 \
-#   --itr 1 --batch_size 32 --learning_rate 0.005 >logs/mirae/$model_name'_'mirae'_'final.log
+#   --itr 1 --batch_size 8 --learning_rate 0.0001 >logs/mirae/$model_name'_'mirae'_'8.log
 
 # # DLinear
 # model_name=DLinear
@@ -73,7 +73,7 @@
 #   --seq_len 72 \
 #   --pred_len 12 \
 #   --train_epochs 10 \
-#   --itr 1 --batch_size 32 --learning_rate 0.005 >logs/mirae/$model_name'_'mirae'_'final.log
+#   --itr 1 --batch_size 8 --learning_rate 0.0001 >logs/mirae/$model_name'_'mirae'_'8.log
 
 # # Transformer
 # model_name=Transformer
@@ -86,17 +86,17 @@
 #   --data mirae \
 #   --des 'Exp' \
 #   --loss mse \
-#   --feature MS \
+#   --feature S \
 #   --seq_len 72 \
 #   --pred_len 12 \
 #   --e_layers 2 \
 #   --d_layers 1 \
 #   --factor 3 \
-#   --enc_in 22 \
-#   --dec_in 22 \
-#   --c_out 22 \
+#   --enc_in 1 \
+#   --dec_in 1 \
+#   --c_out 1 \
 #   --train_epochs 10 \
-#   --itr 1 --batch_size 32 --learning_rate 0.0001 >logs/mirae/$model_name'_'mirae'_'final.log
+#   --itr 1 --batch_size 8 --learning_rate 0.0001 >logs/mirae/$model_name'_'mirae'_'final.log
 
 # # Informer
 # model_name=Informer
@@ -109,63 +109,63 @@
 #   --data mirae \
 #   --des 'Exp' \
 #   --loss mse \
+#   --feature S \
+#   --seq_len 72 \
+#   --pred_len 12 \
+#   --e_layers 2 \
+#   --d_layers 1 \
+#   --factor 3 \
+#   --enc_in 1 \
+#   --dec_in 1 \
+#   --c_out 1 \
+#   --train_epochs 10 \
+#   --itr 1 --batch_size 8 --learning_rate 0.0001 >logs/mirae/$model_name'_'mirae'_'uni.log
+
+# # Autoformer
+# model_name=Autoformer
+# CUDA_VISIBLE_DEVICES=1 python -u run_longExp.py \
+#   --is_training 1 \
+#   --x_data_path ../Data/x_H.npy \
+#   --y_data_path ../Data/y_H.npy \
+#   --model_id mirae_Autoformer \
+#   --model $model_name \
+#   --data mirae \
+#   --des 'Exp' \
+#   --loss mse \
 #   --feature MS \
 #   --seq_len 72 \
 #   --pred_len 12 \
 #   --e_layers 2 \
 #   --d_layers 1 \
 #   --factor 3 \
-#   --enc_in 22 \
-#   --dec_in 22 \
-#   --c_out 22 \
+#   --enc_in 1 \
+#   --dec_in 1 \
+#   --c_out 1 \
 #   --train_epochs 10 \
-#   --itr 1 --batch_size 32 --learning_rate 0.0001 >logs/mirae/$model_name'_'mirae'_'final.log
+#   --itr 1 --batch_size 8 --learning_rate 0.0001 >logs/mirae/$model_name'_'mirae'_'uni.log
 
-# Autoformer
-model_name=Autoformer
-CUDA_VISIBLE_DEVICES=1 python -u run_longExp.py \
-  --is_training 1 \
-  --x_data_path ../Data/x_H.npy \
-  --y_data_path ../Data/y_H.npy \
-  --model_id mirae_Autoformer \
-  --model $model_name \
-  --data mirae \
-  --des 'Exp' \
-  --loss mse \
-  --feature MS \
-  --seq_len 72 \
-  --pred_len 12 \
-  --e_layers 2 \
-  --d_layers 1 \
-  --factor 3 \
-  --enc_in 22 \
-  --dec_in 22 \
-  --c_out 22 \
-  --train_epochs 10 \
-  --itr 1 --batch_size 32 --learning_rate 0.0001 >logs/mirae/$model_name'_'mirae'_'final.log
-
-# SCINet
-model_name=SCINet
-CUDA_VISIBLE_DEVICES=2 python -u run_longExp.py \
-  --is_training 1 \
-  --x_data_path ../Data/x_H.npy \
-  --y_data_path ../Data/y_H.npy \
-  --model_id mirae_SCINet \
-  --model $model_name \
-  --data mirae \
-  --des 'Exp' \
-  --hidden_size 4 \
-  --loss mae \
-  --kernel 5 \
-  --dropout 0.5 \
-  --feature 'S' \
-  --num_stacks 1 \
-  --num_levels 3 \
-  --seq_len 72 \
-  --pred_len 12 \
-  --label_len 12 \
-  --train_epochs 100 \
-  --itr 1 --batch_size 32 --learning_rate 0.0001 >logs/mirae/$model_name'_'mirae'_'final.log
+# # SCINet
+# model_name=SCINet
+# CUDA_VISIBLE_DEVICES=2 python -u run_longExp.py \
+#   --is_training 1 \
+#   --x_data_path ../Data/x_H.npy \
+#   --y_data_path ../Data/y_H.npy \
+#   --model_id mirae_SCINet \
+#   --model $model_name \
+#   --data mirae \
+#   --des 'Exp' \
+#   --hidden_size 4 \
+#   --loss mae \
+#   --kernel 5 \
+#   --dropout 0.5 \
+#   --feature S \
+#   --num_stacks 1 \
+#   --num_levels 3 \
+#   --seq_len 72 \
+#   --pred_len 12 \
+#   --label_len 12 \
+#   --train_epochs 100 \
+#   --itr 1 --batch_size 32 --learning_rate 0.0001 >logs/mirae/$model_name'_'mirae.log
     
 # # # SCINet_decom
 # model_name=SCINet_decom
